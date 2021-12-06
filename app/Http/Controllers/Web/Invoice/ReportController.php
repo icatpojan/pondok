76db092c65bef5880a40b8c71e965a2539ad625d
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-12-06 13:12:45 +0700

    Using tcpd to generate invoice

--- app/Http/Controllers/Web/Invoice/ReportController.php
+++ app/Http/Controllers/Web/Invoice/ReportController.php
@@ -7,6 +7,8 @@ use App\Models\Detail;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use TCPDF;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class ReportController extends Controller
 {
@@ -24,14 +26,50 @@ class ReportController extends Controller
         return view('report.report', compact('title', 'Invoice'));
     }
 
-
     public function cetak_pdf($id)
     {
         $Invoice = Invoice::where('id', $id)->with(['customer', 'ship'])->first();
         $Detail = Detail::where('transaksi_id', $Invoice->id)->with(['product'])->get();
-        $pdf = PDF::loadview('pdf', compact('Invoice', 'Detail'))->setPaper('A4', 'potrait');
-        return $pdf->stream();
+
+        QrCode::format('png')
+            ->merge('/public/img/qrcode.png', .3)
+            ->size(420)
+            ->margin(1)
+            ->generate('https://office.kapalpintar.co.id/cetak/pdf/1', base_path('public/pdf/1.png'));
+
+        $invoiceHtml = view('invoice', compact('Invoice', 'Detail'))->render();
+        $invoicePdf = $this->generate(
+            $invoiceHtml, TRUE, 0, 100, 'FOLIO', array(
+                'left' => 0,
+                'top' => 0,
+                'right' => 0
+            )
+        );
+
+		$invoicePdf->Output($_SERVER['DOCUMENT_ROOT'] . 'pdf/test.pdf', 'F');
+		$invoicePdf->Output('Invoice.pdf', 'I');
     }
+
+	private function generate($html, $watermark = FALSE, $watermark_x = 120, $watermark_y = 330, $paper_size = 'FOLIO', $margin = array('left' => 25, 'top' => 30, 'right' => 25))
+	{
+		$pdf = new TCPDF('P', 'pt', $paper_size, true, 'UTF-8', false);
+
+		$pdf->setPrintHeader(false);
+		$pdf->setPrintFooter(false);
+		$pdf->SetAutoPageBreak(TRUE, 0);
+		$pdf->SetMargins($margin['left'], $margin['top'], $margin['right'], true);
+		$pdf->SetFooterMargin(0);
+		$pdf->AddPage();
+
+		if ($watermark) {
+			$pdf->Image(base_path('public/img/logo-watermark.png'), $watermark_x, $watermark_y, 640, 720, '', '', '', false, false, '', false, false, 0);
+			$pdf->setPageMark();
+		}
+
+		$pdf->writeHTML($html, true, false, true, false, '');
+
+		return $pdf;
+	}
 
     public function delete($id)
     {
